02a61ec8229902035347e7a74275586228d6a5d0
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2022-07-21 19:20:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
+
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragstart', dragend)
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('draenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', dragdrop)
+}
+
+function dragstart(event) {
+    event.target.classList.add('hold')
+    //setTimeout(() => event.target.classList.add('hide'), 0)
+}
+
+function dragend(event) {
+    event.target.className = 'item'
+}
+
+function dragover(event) {
+    event.preventDefault()
+}
+
+function dragenter(event) {
+    event.target.classList.add('hovered')
+}
+
+function dragleave(event) {
+    event.target.classList.remove('hovered')
+}
+
+function dragdrop(event) {
+    event.target.append(item)
+}
